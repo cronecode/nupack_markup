8a9190d256ad66573b4801a1699e2f1c22ba32f4
cronecode <cronecode@gmail.com>
2017-03-08 15:02:36 -0500

    first commit

---
+++ test/calculator.spec.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect
+
+describe('Nupack Markup Calculator', function(){
+    describe('#applyFlatMarkup()', function(){
+        it('adds 5% to the initial base price')
+    })
+
+    describe('#getWorkerMarkup()', function(){
+        it('calculates the worker markup if applicable')
+    })
+
+    describe('#getCategoryMarkup()', function(){
+        it('calculates the category markup if applicable')
+    })
+
+    describe('#nupackPrice()', function(){
+        it('calculates the final cost of the job')
+    })
+})
